dda043b48c577da34fa25d6bc6cea3174345acc6
Heath Shurtleff <heath.shurtleff@overstock.com>
2021-08-25 17:31:39 -0600

    Header routing and auth wiring

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const App = () => {
-    return (
-        <div>
-            Hello There!
-        </div>
-    );
+import Header from './Header';
+import Landing from './Landing';
+
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>Survey New</h2>
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Router>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route path="/surveys/new" component={SurveyNew} />
+                    </div>
+                </Router>
+            </div>
+        );
+    }
 };
 
-export default App;
+export default connect(null, actions)(App);
